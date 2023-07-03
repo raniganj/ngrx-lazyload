b6c6c1e19946524688be66e47ddcbbf8e329722b
sunilmondal4you <sunilmondal4you@gmail.com>
2023-07-03 11:43:58 +0530

    book list view added

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookStore, getAuthorAndBookAction } from '../store/bookstore.reducer';
+import { myStoreState } from '../store/root-reducer';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
-  styleUrls: ['./books.component.scss']
+  styleUrls: ['./books.component.scss'],
 })
-export class BooksComponent {
+export class BooksComponent implements OnInit {
+  bookStore: BookStore = { author: {} };
 
+  constructor(private store: Store<myStoreState>) {}
+
+  ngOnInit(): void {
+    this.store.dispatch(getAuthorAndBookAction());
+    this.store.subscribe((state) => {
+      this.bookStore = state.bookStore;
+    });
+  }
 }
