bbb5569a3b316fe287f1ebf71f19fb812fd13f40
sunilmondal4you <sunilmondal4you@gmail.com>
2023-07-03 16:49:59 +0530

    post reducer added

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { myStoreState } from '../store/root-reducer';
+import { PostModel, getPostListAction } from '../store/post.reducer';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+  styleUrls: ['./post.component.scss'],
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+  postList: PostModel[] | undefined;
+  constructor(private store: Store<myStoreState>) {}
 
+  ngOnInit(): void {
+    this.store.dispatch(getPostListAction());
+    this.store.subscribe((state) => {
+      this.postList = state.postStore.postList;
+    });
+  }
 }
